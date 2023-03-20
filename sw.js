00f15a751a49237b7a585a1f911a18cae2fed2d2
tfgast <tfgast@users.noreply.github.com>
2023-03-20 21:13:58 +0000

    Deploying to gh-pages from @ tfgast/torch_timer@ec5a24ce6fd9419d0a968c57c7e7d6b0b8d08798 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'torch_timer';
+var filesToCache = [
+  './',
+  './index.html',
+  './torch_timer.js',
+  './torch_timer_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
